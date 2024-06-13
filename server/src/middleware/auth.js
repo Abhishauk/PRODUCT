4e14bbf0206c5542746353d716ed957a69bd3e54
Abhishauk <ukabhisha@gmail.com>
2024-06-13 22:54:54 +0530

    updates

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,26 +1,24 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const verifyToken = (req, res, next) => {
-  // Get the token from the request headers
-  const token = req.header('Authorization');
+  const token = req.header("Authorization");
 
-  // Check if token is present
   if (!token) {
-    return res.status(401).json({ msg: 'Authorization denied, no token provided' });
+    return res
+      .status(401)
+      .json({ msg: "Authorization denied, no token provided" });
   }
 
   try {
-    // Verify the token
     jwt.verify(token, process.env.JWT_SECRET);
-    
-    // If token verification is successful, call next middleware or route handler
+
     next();
   } catch (error) {
-    console.error('Token verification error:', error);
-    res.status(401).json({ msg: 'Token is not valid' });
+    console.error("Token verification error:", error);
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
 
